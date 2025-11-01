cb5659dc7bc225e0b03bf5b2a3287ad87909f2ac
Lucas Gonzalez <lucasdragon2001@gmail.com>
2025-10-31 21:09:44 -0300

    Agregar archivos de proyecto.

---
+++ C2_BLL/LogicaNegocios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using C3_DAL;
+using C4_MODEL;
+
+namespace C2_BLL
+{
+    public class LogicaNegocios
+    {
+    }
+}
